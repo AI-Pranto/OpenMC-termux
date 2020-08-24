7960595c62d1f74f11fc8f38dc0f0fde0dfb57b3
Ariful Islam Pranto <pranto.roza.53@gmail.com>
2020-08-24 10:52:30 +0600

    update

--- termux.sh
+++ termux.sh
@@ -10,14 +10,14 @@ mkdir openmc_root
 cd openmc
 mkdir bld
 cd bld
-cmake -DCMAKE_INSTALL_PREFIX=/data/data/com.termux/files/home/openmc_root ..
+cmake -DCMAKE_INSTALL_PREFIX=$HOME/openmc_root ..
 make -j 8 # number of core
 make install
-echo 'export PATH=/data/data/com.termux/files/home/openmc_root/bin:$PATH' >> $PREFIX/etc/bash.bashrc
+echo 'export PATH=$HOME/openmc_root/bin:$PATH' >> $PREFIX/etc/bash.bashrc
 cd
 wget https://anl.box.com/shared/static/teaup95cqv8s9nn56hfn7ku8mmelr95p.xz
 tar -xvf teaup95cqv8s9nn56hfn7ku8mmelr95p.xz
-echo 'export OPENMC_CROSS_SECTIONS=/data/data/com.termux/files/home/nndc_hdf5/cross_sections.xml' >> $PREFIX/etc/bash.bashrc
+echo 'export OPENMC_CROSS_SECTIONS=$HOME/nndc_hdf5/cross_sections.xml' >> $PREFIX/etc/bash.bashrc
 source $PREFIX/etc/bash.bashrc
 rm -r teaup95cqv8s9nn56hfn7ku8mmelr95p.xz
 cd OpenMC-termux
