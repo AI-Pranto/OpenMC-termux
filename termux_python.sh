144ab4b02b21ab8438a9eab315e3ddb5343740d5
Ariful Islam Pranto <pranto.roza.53@gmail.com>
2021-12-17 22:45:45 +0600

    update for python support

--- termux_python.sh
+++ termux_python.sh
@@ -1,67 +1,75 @@
+#!/bin/bash
+
+clear
 pkg update && pkg upgrade -y
 
 wget -P $HOME https://its-pointless.github.io/setup-pointless-repo.sh
 bash $HOME/setup-pointless-repo.sh
 
 pkg install build-essential \
-	cmake \
 	clang \
+	cmake \
+	fftw \
+	freetype \
+	libpng \
+	libzmq \
 	gcc-10 \
+	git \
 	libgfortran \
-	wget \
+	libgmp \
+	libmpc \
+	libmpfr \
 	libhdf5-static \
-	vim \
-	python \
 	libxml2 \
 	libxslt \
-	libjpeg-turbo -y
+	libjpeg-turbo \
+	pkg-config \
+	python \
+	vim \
+	wget \
+	zlib -y
 
 # setup gcc
 setupgcc-10
 
 pip install --upgrade pip
+pkg install numpy \
+			scipy
 
-# numpy and scipy install
-pip install wheel \
-	scipy \
-	numpy
+# numpy, scipy, cython, h5py, lxml, ipython install
+pip install cython \
+			ipython \
+			lxml \
+			h5py \
+			numpy \
+			scipy
 
 # pandas
 export CFLAGS="-Wno-deprecated-declarations -Wno-unreachable-code" && pip install pandas
+LDFLAGS=" -lm -lcompiler_rt" pip install pandas
 
 # matplotlib
-cd ~
-git clone https://github.com/matplotlib/matplotlib.git
-cd matplotlib
-sed 's@#enable_lto = True@enable_lto = False@g' setup.cfg.template > setup.cfg
-pip install .
-
-# lxml
-pip install lxml \
-	h5py
+CFLAGS=" -I/data/data/com.termux/files/usr/include/freetype2" CPPFLAGS=$CFLAGS LDFLAGS=" -lm -lcompiler_rt" pip install matplotlib
 
 # pillow
 LDFLAGS="-L/system/lib/" CFLAGS="-I/data/data/com.termux/files/usr/include/" pip install Pillow
 
+# openmc install
 mkdir -p $HOME/opt/OpenMC/build && cd ~/opt/OpenMC
 git clone --recurse-submodules https://github.com/openmc-dev/openmc.git
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$HOME/opt/OpenMC ../openmc
-make -j 8 install
+make -j${nproc --all} install
 
-# Env for OpenMC
+# Env set for OpenMC
 echo 'export PATH=$HOME/opt/OpenMC/bin:$PATH' >> $PREFIX/etc/bash.bashrc
 echo 'export LD_LIBRARY_PATH=$HOME/opt/OpenMC/lib:$LD_LIBRARY_PATH' >> $PREFIX/etc/bash.bashrc
 
 # Download HDF5 data
-if [[ -z "${OPENMC_CROSS_SECTIONS}" ]]; then
-	wget -c -P $HOME https://anl.box.com/shared/static/teaup95cqv8s9nn56hfn7ku8mmelr95p.xz -O nndc
-	tar -xvf $HOME/nndc -C $HOME
-fi
+wget -q -O - https://anl.box.com/shared/static/teaup95cqv8s9nn56hfn7ku8mmelr95p.xz | tar -C $HOME -xJ
 
 echo 'export OPENMC_CROSS_SECTIONS=$HOME/nndc_hdf5/cross_sections.xml' >> $PREFIX/etc/bash.bashrc
 source $PREFIX/etc/bash.bashrc
-rm -rf $HOME/nndc
 
 # python API
 cd ~/opt/OpenMC/openmc
