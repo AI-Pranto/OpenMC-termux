eebcde334315e3d340807874f31cd996f3303358
Ariful Islam Pranto <pranto.roza.53@gmail.com>
2021-12-20 00:16:24 +0600

    small update

--- termux.sh
+++ termux.sh
@@ -1,15 +1,18 @@
+#!/bin/bash
+
+clear
+
 pkg update && pkg upgrade -y
 
-# Dependency
+# Dependency install
 pkg install cmake \
 	clang \
 	wget \
 	libhdf5-static \
-	vim
+	vim -y
 
+# openmc install
 mkdir -p $HOME/openmc_root
-
-# git clone openmc and install
 git clone --recurse-submodules https://github.com/openmc-dev/openmc.git
 cd openmc && mkdir bld && cd bld
 cmake -DCMAKE_INSTALL_PREFIX=$HOME/openmc_root ..
@@ -17,10 +20,9 @@ make -j 8 install
 echo 'export PATH=$HOME/openmc_root/bin:$PATH' >> $PREFIX/etc/bash.bashrc && cd ~
 
 # Download data library
-wget https://anl.box.com/shared/static/teaup95cqv8s9nn56hfn7ku8mmelr95p.xz -O nndc
-tar -xvf nndc
+wget -q -O - https://anl.box.com/shared/static/teaup95cqv8s9nn56hfn7ku8mmelr95p.xz | tar -C $HOME -xJ
 echo 'export OPENMC_CROSS_SECTIONS=$HOME/nndc_hdf5/cross_sections.xml' >> $PREFIX/etc/bash.bashrc
-source $PREFIX/etc/bash.bashrc && rm -rf nndc
+source $PREFIX/etc/bash.bashrc
 
-# Run test
+# Run pin cell problem
 cd $HOME/OpenMC-termux/xml-file && openmc
